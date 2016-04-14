187d74585143cdf05e20d3601916d055c4b3329d
福森翔太 <fukumori.syouta@gmail.com>
2016-04-15 00:18:41 +0900

    ユーザーログイン開発

--- login.php
+++ login.php
@@ -1,36 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<!DOCTYPE HTML>
- <html lang="ja">
- <head>
- 	<meta charset="utf-8">
- 	<link rel="stylesheet" type="text/css" href="style.css">
- 	<title></title>
- </head>
- <style>
-
-
- </style>
- <div id="header">
- 	<div id="header_nav">
-
- 	</div>
- </div>
- <?php 
- require('body_index.php');
- ?>
-
-=======
-kagosima
->>>>>>> login
-=======
 <?php
 class Controller
 {
-	public function action_index($a)
-	{		
+	public function action_index()
+	{
 		
 	}
 }
+
 ?>
->>>>>>> 1685762c607ba5fd110b4925757a7d77efa01160
