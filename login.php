c56342899470cc76b902b433965caf7cd13a1a5d
福森翔太 <fukumori.syouta@gmail.com>
2016-04-17 19:00:44 +0900

    login機能作成

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+ <html lang="ja">
+ <head>
+ 	<meta charset="utf-8">
+ 	<link rel="stylesheet" type="text/css" href="style.css">
+ 	<title></title>
+ </head>
+ <style>
+
+
+ </style>
+ <div id="header">
+ 	<div id="header_nav">
+ 	<table border="1">
+ 	<tr>
+ 		<th id="login">ログイン</th>
+ 		<th id="otoi">お問い合わせ</th>
+ 	</tr>
+ 	</table>
+ 	</div>
+ </div>
+ <?php 
+ require('body_index.php');
+ ?>
+
