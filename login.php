3fa8a9dd9c9862a0a4f0c4f1ae749d822eefb994
福森翔太 <fukumori.syouta@gmail.com>
2016-04-17 18:47:50 +0900

    test

--- login.php
+++ login.php
@@ -1,1 +1,24 @@
+<<<<<<< 34fd8a81c058686aa39f640d1785d7d93a9b5c31
 kagosima
+=======
+<!DOCTYPE HTML>
+ <html lang="ja">
+ <head>
+ 	<meta charset="utf-8">
+ 	<link rel="stylesheet" type="text/css" href="style.css">
+ 	<title></title>
+ </head>
+ <style>
+
+
+ </style>
+ <div id="header">
+ 	<div id="header_nav">
+
+ 	</div>
+ </div>
+ <?php 
+ require('body_index.php');
+ ?>
+
+>>>>>>> login
