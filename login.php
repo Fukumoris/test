439beb0442dc8375e5bdab769008418a86d7a030
福森翔太 <fukumori.syouta@gmail.com>
2016-04-17 18:49:10 +0900

    test

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<<<<<<< 3fa8a9dd9c9862a0a4f0c4f1ae749d822eefb994
 <<<<<<< 34fd8a81c058686aa39f640d1785d7d93a9b5c31
 kagosima
 =======
@@ -22,3 +23,15 @@ kagosima
  ?>
 
 >>>>>>> login
+=======
+<?php
+class Controller
+{
+	public function action_index()
+	{
+		
+	}
+}
+
+?>
+>>>>>>> ユーザーログイン開発
